889fef5939a3444498405554bc95e29013932a10
Kelly Walsh <kellywalsh827@gmail.com>
2021-11-22 21:02:57 -0700

    fixed css styling

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # day-planner
+***
+### This project was an activity on creating my own online web developer portfolio. Implementing the various things we have learned in this course to create an interactive and attractive portfoilo.
+
+![My portfolio includes a navigation bar, images, and several headers, sections and cards.](./assets/Images/PortfolioScreenshot.png)
+
+***
+
+[KMW-Portfolio Website](https://kemwalsh.github.io/KEM-PORTFOLIO/)
--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,43 @@
-//Date and Time
-var myClock = document.getElementById("currentDay");
-myClock.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
-setInterval(function () {
-    myClock.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
-    console.log("anything")
-}, 1000)
+// Date and Time  TURN INTO JQUERY
+var myClock = document.getElementById("currentDay").textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
+// console.log(myClock)
+// myClock.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
+// setInterval(function () {
+//     myClock.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
+//     // console.log("anything")
+// }, 1000)
 
+
+let time = moment();
+
+$(".saveBtn").click(function () {
+    const time = $(this).parent().attr("id");
+    const schedule = $(this).siblings(".schedule").val()
+    localStorage.setItem(time, schedule);
+})
+
+function startPage() {
+    $(".time-block").each(function () {
+        const lsData = localStorage.getItem($(this).attr("id"));
+
+        if (lsData) {
+            $(this).children(".schedule").val(lsData);
+        }
+        setCalendarColors($(this).attr("id"), this);
+    })
+}
+
+function setCalendarColors(savedTime, element) {
+    hour = time.hours();
+    console.log(savedTime);
+    if (parseInt(savedTime) > hour) {
+        $(element).addClass("future");
+    }
+    else if (parseInt(savedTime) < hour) {
+        $(element).addClass("past");
+    }
+    else {
+        $(element).addClass("present");
+    }
+}
+startPage();
